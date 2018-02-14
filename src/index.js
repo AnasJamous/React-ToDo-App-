75d8a96c82a9f85678408c9e7bc30591bc8f1e3e
AnasJamous <anas.jam89@gmail.com>
2018-02-14 12:49:02 +0200

    todo App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import App from './component/app';
+import ReactDOM from 'react-dom';
+
+
+const root = document.getElementById('app')
+ReactDOM.render (<App / >, root);
